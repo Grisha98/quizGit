dd47b7f888b10e668d4deda147e7ce8f1b793be0
Grisha98 <grisha.workstudy@gmail.com>
2017-07-14 12:56:39 +0400

    updated register file

--- register.php
+++ register.php
@@ -1,4 +1,10 @@
 <?php
 
-   echo "You have CORS!";
+  include 'db/query.php';
+
+  $_POST['password']=password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+  $dbCall->insert('Users', $_POST);
+
+
 ?>
